47aa0ffd97afbfb227cdf6347028f8c20d9f2461
AsyncAwaitYash <yashfors91@gmail.com>
2026-08-01 00:51:13 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+
+        unordered_map<int, int> map;
+
+        for(int i = 0; i< nums.size(); i++){
+            int requiredSum = target - nums[i];
+            if(map.count(requiredSum)){
+                ans.push_back(map[requiredSum]);
+                ans.push_back(i);
+                return ans;
+            }
+
+            map[nums[i]] = i;
+        }
+        
+        return ans;
+    }
+};
